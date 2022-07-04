72b3f7731b0e9d43fe8884d5ce880c1635d59ccd
Jacob Westaway <jw1542@gadi-login-05.gadi.nci.org.au>
2022-07-04 14:30:51 +1000

    Updates

--- scripts/05_Variant_filtering/Plot_quality_scores.R
+++ scripts/05_Variant_filtering/Plot_quality_scores.R
@@ -15,7 +15,8 @@ VCF <- read.csv(snp_filepath, header = T, na.strings=c("","NA"), sep = "\t") %>%
 
 QD <- ggplot(VCF, aes(x=QD, fill=Variant)) + 
   geom_density(alpha=.3) +
-  geom_vline(xintercept=20, size=0.7)
+  geom_vline(xintercept=20, size=0.7) + 
+  coord_cartesian(xlim = c(10,45))
 
 FS <- ggplot(VCF, aes(x=FS, fill=Variant)) + 
   geom_density(alpha=.3) +
@@ -23,7 +24,8 @@ FS <- ggplot(VCF, aes(x=FS, fill=Variant)) +
 
 MQ <- ggplot(VCF, aes(x=MQ, fill=Variant)) + 
   geom_density(alpha=.3) +
-  geom_vline(xintercept=59, size=0.7) 
+  geom_vline(xintercept=59, size=0.7) + 
+  coord_cartesian(xlim = c(58,70)) 
 
 grid.arrange(QD, FS, MQ)
 }
